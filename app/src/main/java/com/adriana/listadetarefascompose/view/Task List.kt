750f459b21f0bc672716e82e0649fd61a500b3e9
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-04-20 12:10:50 -0300

    feat: adding navigation screens and configuring

--- app/src/main/java/com/adriana/listadetarefascompose/view/Task List.kt
+++ app/src/main/java/com/adriana/listadetarefascompose/view/Task List.kt
@@ -1,12 +1,57 @@
 package com.adriana.listadetarefascompose.view
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.adriana.listadetarefascompose.R
+import com.adriana.listadetarefascompose.ui.theme.BLACK
+import com.adriana.listadetarefascompose.ui.theme.Purple200
+import com.adriana.listadetarefascompose.ui.theme.Purple700
+import com.adriana.listadetarefascompose.ui.theme.WHITE
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun TaskList(
     navController: NavController
 ){
-    Text(text = "Lista de Tarefas")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Purple200,
+                title = {
+                    Text(
+                        text = "Lista de Tarefas",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = WHITE
+
+                    )
+                }
+            )
+        },
+        backgroundColor = BLACK,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+
+                },
+                backgroundColor = Purple200
+            ) {
+                Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
+                    contentDescription = "Ícone de Salvar Tarefas!"
+                )
+            }
+        }
+        ) {
+
+    }
 }
