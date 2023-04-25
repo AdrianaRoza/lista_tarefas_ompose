944a33e8ec3756bb054ef4440520ad3b5132c05d
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-04-25 16:01:30 -0300

    feat: finishing creating fake tasks to validate the task list

--- app/src/main/java/com/adriana/listadetarefascompose/itemlist/TaskItem.kt
+++ app/src/main/java/com/adriana/listadetarefascompose/itemlist/TaskItem.kt
@@ -1,13 +1,11 @@
 package com.adriana.listadetarefascompose.itemlist
 
+import android.renderscript.RenderScript.Priority
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,11 +15,48 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.adriana.listadetarefascompose.R
-import com.adriana.listadetarefascompose.ui.theme.Purple
-import com.adriana.listadetarefascompose.ui.theme.ShapeCardPriority
+import com.adriana.listadetarefascompose.model.Task
+import com.adriana.listadetarefascompose.ui.theme.*
 
 @Composable
-fun TaskItem(){
+fun TaskItem(
+    position: Int,
+    todoList: MutableList<Task>
+){
+
+    val titleTask = todoList[position].task
+    val descriptionTask = todoList[position].description
+    val priorityTask = todoList[position].priority
+
+    var priorityLevel: String = when(  priorityTask){
+        0 -> {
+            "Sem prioridade"
+        }
+        1 -> {
+            "Prioridade Baixa"
+        }
+        2 -> {
+            "Prioridade Média"
+        }
+        else -> {
+            "Prioridade Alta"
+        }
+    }
+
+    var color = when(priorityTask){
+        0 -> {
+            Color.Black
+        }
+        1 -> {
+            RADIO_BUTTON_GREEN_SELECTED
+        }
+        2 ->{
+            RADIO_BUTTON_YELLOW_SELECTED
+        }
+        else -> {
+            RADIO_BUTTON_RED_SELECTED
+        }
+    }
 
     Card(
         backgroundColor = Purple,
@@ -37,7 +72,7 @@ fun TaskItem(){
             val (txtTitle,txtDescription,cardPriority,txtPriority,btDelete) = createRefs()
 
             Text(
-                text = "Task 1",
+                text = titleTask.toString(),
                 modifier = Modifier.constrainAs(txtTitle){
                     top.linkTo(parent.top, margin = 10.dp)
                     start.linkTo(parent.start, margin = 10.dp)
@@ -46,7 +81,7 @@ fun TaskItem(){
             )
 
             Text(
-                text = "asgagsgasasasasasasagsaggahsgshg",
+                text = descriptionTask.toString(),
                 modifier = Modifier.constrainAs(txtDescription){
                     top.linkTo(txtTitle.bottom, margin = 10.dp)
                     start.linkTo(parent.start, margin = 10.dp)
@@ -55,7 +90,7 @@ fun TaskItem(){
             )
 
             Text(
-                text = "Prioridade Baixa",
+                text = priorityLevel,
                 modifier = Modifier.constrainAs(txtPriority){
                     top.linkTo(txtDescription.bottom, margin = 10.dp)
                     start.linkTo(parent.start, margin = 10.dp)
@@ -64,7 +99,7 @@ fun TaskItem(){
             )
 
             Card(
-                backgroundColor = Color(0xFF318635),
+                backgroundColor = color,
                 modifier = Modifier
                     .size(30.dp)
                     .constrainAs(cardPriority) {
--- app/src/main/java/com/adriana/listadetarefascompose/view/Task List.kt
+++ app/src/main/java/com/adriana/listadetarefascompose/view/Task List.kt
@@ -2,6 +2,8 @@ package com.adriana.listadetarefascompose.view
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -13,6 +15,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.adriana.listadetarefascompose.R
+import com.adriana.listadetarefascompose.itemlist.TaskItem
+import com.adriana.listadetarefascompose.model.Task
 import com.adriana.listadetarefascompose.ui.theme.BLACK
 import com.adriana.listadetarefascompose.ui.theme.Purple200
 import com.adriana.listadetarefascompose.ui.theme.WHITE
@@ -37,7 +41,7 @@ fun TaskList(
                 }
             )
         },
-        backgroundColor = BLACK,
+        backgroundColor = WHITE,
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
@@ -53,5 +57,35 @@ fun TaskList(
         }
         ) {
 
+        val todoList: MutableList<Task> = mutableListOf(
+            Task(
+                task = "Ler",
+                description = "hdjshfahshfajsh",
+                priority = 0
+            ),
+
+            Task(
+                task = "Ir ao cinema",
+                description = "hdjshfahshfajsh",
+                priority = 1
+            ),
+
+            Task(
+                task = "Estudar",
+                description = "hdjshfahshfajsh",
+                priority = 2
+            ),
+
+            Task(
+                task = "Se exercitar",
+                description = "hdjshfahshfajsh",
+                priority = 3
+            )
+        )
+        LazyColumn{
+            itemsIndexed(todoList){position, _ ->
+                TaskItem(position, todoList)
+            }
+        }
     }
 }
